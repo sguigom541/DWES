2c3a66f87ad9a2def8d165fd72ff2608e431cda1
sguigom541 <2daasguigom21@ieslasfuentezuelas.com>
2021-01-26 01:24:41 +0100

    symfony

--- Tema8/SOADALUMNOS/SERVICIOSOAD/crearwsdl.php
+++ Tema8/SOADALUMNOS/SERVICIOSOAD/crearwsdl.php
@@ -1,12 +1,12 @@
 <?php
-require_once './vendor/autoload.php';
+/*require_once './vendor/autoload.php';
 include './servicioalumno.php';
 $class = 'servicioalumno';
 $serviceURI = 'http://localhost/2DAW/SOADALUMNOS/SERVICIOSOAD/';
-$wsdlGenerator = new php2
+$wsdlGenerator = new php2;
 // Generate the WSDL from the class adding only the public methods that have @soap annotation.
 $wsdlGenerator->generateWSDL(true);
 // Dump as string
 $wsdlXML = $wsdlGenerator->dump();
 // Or save as file
-$wsdlXML = $wsdlGenerator->save('saludo.wsdl');
+$wsdlXML = $wsdlGenerator->save('saludo.wsdl');*/
